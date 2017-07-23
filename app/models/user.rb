88cff93e98bf183ac98a85b745171140047352d8
DucLinh0305 <duclinh0305@gmail.com>
2017-07-23 19:23:40 +0700

    Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,23 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.password_minimum_length}
 
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      if cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+
   private
 
   def downcase_email
