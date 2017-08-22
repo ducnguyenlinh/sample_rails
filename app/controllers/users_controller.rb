30ca1226e35f507fbc831fb01860c6636856559f
ducnguyenlinh <duclinh0305@gmail.com>
2017-08-22 10:39:22 +0700

    Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate page: params[:page]
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
 
   def new
@@ -16,15 +16,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-
     if user.save
-      save_user user
+      @user.send_activation_email
+      flash[:info] = t "mailer.info"
+      redirect_to root_url
     else
       render :new
     end
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
+  end
 
   def edit; end
 
